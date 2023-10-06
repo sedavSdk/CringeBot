63929a649239cc095152e3272280d4756400ad1f
Matvey <severov555@gmail.com>
2023-10-06 22:01:42 +0300

    add permissions and logs

--- utils.py
+++ utils.py
@@ -1,3 +1,11 @@
+from discord.utils import get
+
 def is_connected(voice_client):
     return voice_client and voice_client.is_connected()
 
+def check_ban(interaction, role):
+    role = get(interaction.guild.roles, name=role)
+    if not role:
+        return False
+    roles = interaction.user.roles
+    return role in roles
